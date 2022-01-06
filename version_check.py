6b850fa7fdfe659394734df5fb47ce740228f4e2
Eniola Emmanuel Olawale <emmanomax33@gmail.com>
2022-01-06 16:45:53 +0100

    Trying out nested widgets and layouts in pyqt6

--- version_check.py
+++ version_check.py
@@ -18,9 +18,11 @@ from PyQt6.QtWidgets import (
     QApplication,
     QHBoxLayout,
     QVBoxLayout,
+    QTabWidget,
     QWidget,
     QPushButton,
 )
+
 
 class Window(QWidget):
     def __init__(self):
@@ -29,27 +31,53 @@ class Window(QWidget):
         self.screen_height = 768
         self.setWindowTitle("Manhua Reader")
 
-        # Create a vertical layout
-        """
-        layout = QVBoxLayout()
-        # Add widgets to the layout
-        layout.addWidget(QPushButton("top"),0)
-        layout.addWidget(QPushButton("middle"), 2)
-        layout.addWidget(QPushButton("buttom"), 0)
-        """
+       """ #-----------------------------------------------------------------------------
+        # Create a vertical centralLayout
+        centralLayout = QVBoxLayout()
+        self.setLayout(centralLayout)
 
-        # Create an horizontal layout
-        #"""
-        layout = QHBoxLayout()
-        # Add widgets to the layout
-        layout.addWidget(QPushButton("Menu"), 0)
-        layout.addWidget(QPushButton("Enter Keyword"), 2)
-        layout.addWidget(QPushButton("Search online"), 0)
-        layout.addWidget(QPushButton("local Search"), 0)
-        #"""
+        #-----------------------------------------------------------------------------
+        # Create an horizontal searchLayout
+        searchLayout = QHBoxLayout()
+        # Add widgets to the searchLayout
+        searchLayout.addWidget(QPushButton("Menu"), 0)
+        searchLayout.addWidget(QPushButton("Enter Keyword"), 2)
+        searchLayout.addWidget(QPushButton("Search online"), 0)
+        searchLayout.addWidget(QPushButton("local Search"), 0)
+                
+        #-----------------------------------------------------------------------------   
+        # Create an vertical homeLayout
+        homeLayout = QVBoxLayout()
+        # Create the tabWidget in homeLayout
+        tabWidget = QTabWidget()
+        # Set Position of tabBar to left
+        # tabWidget.setTabPosition(QTabWidget.West)
+        # Add tabs to the tabWidget
+        tabWidget.addTab(self.genHomeTab(), "Home")
+        tabWidget.addTab(self.genLibraryTab(), "Library")
+        # Add tabWidget to the homeLayout
+        homeLayout.addWidget(tabWidget)
+        #-----------------------------------------------------------------------------
+
+        # Create an vertical historyLayout
+        historyLayout = QVBoxLayout()
+
+        #-----------------------------------------------------------------------------
+        # Create an horizontal containerLayout
+        containerLayout = QHBoxLayout()
+        # Add widgets to the containerLayout
+        containerLayout.addChildLayout(homeLayout)
+        containerLayout.addChildLayout(historyLayout)
 
         # Add Layout to parent
-        self.setLayout(layout)
+        centralLayout.addChildLayout(searchLayout)
+        centralLayout.addChildLayout(containerLayout)"""
+
+    def genHomeTab(self):
+            pass
+
+    def genLibraryTab(self):
+            pass
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
