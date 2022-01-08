7c31080f325837b1120939585622c70b0569537a
Eniola Emmanuel Olawale <emmanomax33@gmail.com>
2022-01-08 23:29:49 +0100

    More

--- MangaReader/src/main.py
+++ MangaReader/src/main.py
@@ -9,40 +9,106 @@ from PyQt6.QtWidgets import (
     QLineEdit,
     QLabel,
     QListView,
+    QSizePolicy,
+    QStatusBar,
     QPushButton,
 )
-
+from PyQt6.QtGui import QCursor, QIcon, QPixmap
 from PyQt6.QtCore import QSize, Qt
 
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.screen_width = 1366
-        self.screen_height = 768
-        self.min_screen_width = 1092
-        self.min_screen_height = 614
+        #self.screen_width = 1366
+        #self.screen_height = 768
+        self.min_screen_width = 915
+        self.min_screen_height = 515
+        self.resize_width = 1092
+        self.resize_height = 614
+        self.max_button_size = QSize(50, 50)
+        self.min_button_size = QSize(16, 16)
+        self.icon_size = QSize(35, 35)
         self.appWindowTitle = "Manhua Reader"
+        self.resize(QSize(self.resize_width, self.resize_height))
         self.setMinimumSize(QSize(self.min_screen_width, self.min_screen_height))
         self.setWindowTitle(self.appWindowTitle)
+
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+
+
         #------------------------------------------------
         self.centralwidget = QWidget()
+
+
         #------------------------------------------------
         self.gridLayout = QGridLayout(self.centralwidget)
+
+
         #------------------------------------------------
         # Create a centralLayout to hold all other layouts
         self.centralLayout = QVBoxLayout()
+        self.centralLayout.setSpacing(9)
+
+
+
         #------------------------------------------------
         # Create an horizontal layout to hold all search functions
         self.searchLayout = QHBoxLayout()
+        self.searchLayout.setContentsMargins(-1, 10, -1, 10)
         # Create search function widgets
+
+
+
         # Menu/Option button 
         self.menuButton = QPushButton()
+        sizePolicy.setHeightForWidth(self.menuButton.sizePolicy().hasHeightForWidth())
+        self.menuButton.setSizePolicy(sizePolicy)
+        self.menuButton.setMinimumSize(self.min_button_size)
+        self.menuButton.setMaximumSize(self.max_button_size)
+        self.menuButton.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
+        menuIcon = QIcon()
+        menuIcon.addPixmap(QPixmap("MangaReader/resources/icons/icons8-menu-96.png"), QIcon.Mode.Normal, QIcon.State.Off)
+        self.menuButton.setIcon(menuIcon)
+        self.menuButton.setIconSize(self.icon_size)
+
+
+
         # TextBox
         self.lineEdit = QLineEdit()
+        self.lineEdit.setPlaceholderText("Enter Search keyword here...")
+        sizePolicy.setHeightForWidth(self.lineEdit.sizePolicy().hasHeightForWidth())
+        self.lineEdit.setSizePolicy(sizePolicy)
+        self.lineEdit.setMinimumSize(QSize(0, 50))
+        self.lineEdit.setMaximumSize(QSize(1366, 50))
+        self.lineEdit.setMaxLength(36)
+        self.lineEdit.setClearButtonEnabled(True)
+
+        
+
         # Search Button
         self.searchButton = QPushButton()
+        sizePolicy.setHeightForWidth(self.searchButton.sizePolicy().hasHeightForWidth())
+        self.searchButton.setSizePolicy(sizePolicy)
+        self.searchButton.setMinimumSize(self.min_button_size)
+        self.searchButton.setMaximumSize(self.max_button_size)
+        self.searchButton.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
+        searchIcon = QIcon()
+        searchIcon.addPixmap(QPixmap("MangaReader/resources/icons/icons8-search-90.png"), QIcon.Mode.Normal, QIcon.State.Off)
+        self.searchButton.setIcon(searchIcon)
+        self.searchButton.setIconSize(self.icon_size)
+
+        
+
         # Local Search
         self.localSearchButton = QPushButton()
+        sizePolicy.setHeightForWidth(self.localSearchButton.sizePolicy().hasHeightForWidth())
+        self.localSearchButton.setSizePolicy(sizePolicy)
+        self.localSearchButton.setMinimumSize(self.min_button_size)
+        self.localSearchButton.setMaximumSize(self.max_button_size)
+        self.localSearchButton.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
+
+
+
         # Add Widgets to searchLayout
         self.searchLayout.addWidget(self.menuButton)
         self.searchLayout.addWidget(self.lineEdit)
@@ -78,7 +144,46 @@ class Window(QMainWindow):
         #------------------------------------------------
         self.gridLayout.addLayout(self.centralLayout, 0, 0, 1, 1)
 
+
+        #------------------------------------------------
+
+        self.statusbar = QStatusBar(self)
+        self.setStatusBar(self.statusbar)
+        
+
+        #-----------------------------------------------
+        self.style = """
+                QTabWidget{
+                    border: 1px solid rgba(0,0,0,40);
+                }
+                QPushButton{
+                    border-radius:25px;
+                }
+                QPushButton:hover {
+                    background-color: rgba(0,0,0,40);
+                    color: white;
+                }
+                QLineEdit{
+                    border: 1px solid rgba(0,0,0,40);
+                    border-radius: 25px;
+                    padding-left: 25px;
+                    font: 13px;
+                }
+                QStatusBar{
+                    background-color: rgba(0,0,0,40);
+                }
+            """
+        self.setStyleSheet(self.style)
+        #-----------------------------------------------
+        self.searchButton.clicked.connect(self.search)
+        self.lineEdit.returnPressed.connect(self.searchButton.click)
+        
+
+
+        #-----------------------------------------------
+
         self.setCentralWidget(self.centralwidget)
+        
 
     def create_home_widgets(self):
         self.tabWidget = QTabWidget()
@@ -108,3 +213,12 @@ class Window(QMainWindow):
     def create_stack_widget_library(self):
         #create_library_widgets()
         pass
+
+    def search(self):
+        self.keyword = self.lineEdit.text()
+        if self.keyword != "":
+            print(self.keyword)
+
+
+    
+
